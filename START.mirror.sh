0e75f6128af3d81fb58cc18cfdd2d460fd10c966
Daniel Morfa Vega <dmorfav@gmail.com>
2019-03-13 17:50:19 -0400

    Reconstruyendo script

--- START.mirror.sh
+++ START.mirror.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
-#
-# Script principal para la puesta en funcionamiento
-# del Mirror local de Kali Linux
-#
-# Nombre del Archivo: START.mirror.sh
-# Fecha: 08-11-2017
-# Version 1.0
+
+####################################################
+# 	Nombre del Archivo: START.mirror.sh        #
+# 	Fecha: 08-11-2017			   #
+# 	Version 1.0				   #
 ####################################################
 
-# Directorio de Descarga
-rutaWork='/home/ronadmin/Kalimirror'
-cd $rutaWork
-# Script responsable del repositorio
-main='Kali.Main.sh'
 
-/bin/bash $rutaWork/$main 2> $rutaWork/error_mirror.log
-sleep 10
-exit
+echo "Script para Mirrors a distribuciones linux"
+echo "Seleccione una distribución"
+echo "####################################################"
+echo "# 1-Kali                                           #"
+echo "# 2-Debian                                         #"
+echo "####################################################"
+read distro 
+
+# Script responsable del repositorio
+
+ #Directorio donde se almacenan los repositorios
+ DIR="$HOME/mirror" 
+
+mkdir $DIR
+
+if [ $? -eq 0 ];
+    then
+    echo "$DIR ha sido creado"
+else
+    echo "$DIR no pudo ser creado"
+fi
+
+case $distro in
+     1 )
+	main='Kali.Main.sh'
+	echo "Creado directorio $DIR/kali"
+	mkdir -p $DIR/kali
+	/bin/bash $main 2> $DIR/kali/error_mirror.log
+	;;
+     2 ) 
+	main='Debian.Main.sh'
+	echo "Creado directorio $DIR/debian"
+	mkdir -p $DIR/debian
+	/bin/bash $main 2> $DIR/debian/error_mirror.log
+	;;
+esac  
