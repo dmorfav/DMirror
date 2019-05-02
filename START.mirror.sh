4c378a414ed0346d90b957abd8bd7d95b26ddd79
Daniel Morfa Vega <dmorfav@gmail.com>
2019-05-02 09:27:42 -0400

    Modificando script para recibir parametros

--- START.mirror.sh
+++ START.mirror.sh
@@ -6,47 +6,27 @@
 # 	Version 1.0				   #
 ####################################################
 
-echo "Script para Mirrors a distribuciones linux"
-echo "Seleccione una distribución"
-echo "####################################################"
-echo "# 1-Kali                                           #"
-echo "# 2-Debian                                         #"
-echo "####################################################"
-read distro 
-
 # Script responsable del repositorio 
 function makeDir(){
 #Directorio donde se almacenan los repositorios
 DIR="$HOME/mirror";
-#//TODO revisar porque da error esta condicion
-if [$# -eq 0];
-   then 
-	mkdir $DIR >/dev/null 2>&1
+	mkdir $DIR/$1 >/dev/null 2>&1
 	if [ $? -eq 0 ];
     		then
     			echo "$DIR ha sido creado"
 		else
     			echo "$DIR no pudo ser creado porque ya existe"
 	fi
-else 
-	mkdir $DIR/$1 >/dev/null 2>&1
-	if [ $? -eq 0 ];
-    		then
-    			echo "$DIR/$1 ha sido creado"
-		else
-    			echo "$DIR/$1 no pudo ser creado porque ya existe"
-	fi
-fi
 }
 
-case $distro in
-     1 )
+case $1 in
+     "kal" )
 	main='./distro/Kali.Main.sh'
 	makeDir	
-	makeDir kali
+	makeDir Kali
 	/bin/bash $main 2> $HOME/mirror/kali/error_mirror.log
 	;;
-     2 ) 
+     "deb" ) 
 	main='./distro/Debian.Main.sh'
 	makeDir	
 	makeDir debian
