e9d822f6419dfeb16793e2540785e7d1bbe7464d
Daniel Morfa Vega <dmorfav@gmail.com>
2019-05-02 10:04:25 -0400

    Generando script para descarga

--- distro/Debian.Main.sh
+++ distro/Debian.Main.sh
@@ -1,18 +1,30 @@
-#!/bin/sh
-debmirror -debug \
--progress \
--verbose \
--diff=none \
--host=deb.debian.org/debian \
--root=/ \
--method=http \
--dist=stretch \
--arch=i386,amd64 \
--nosource \
--section=main,contrib,non-free \
--i18n \
--getcontents \
--ignore-release-gpg \
--ignore-missing-release \
-$HOME/mirror/debian
-exit
+#!/bin/bash
+ 
+# sourcehost: choose a mirror in your proximity!
+HOST=ftp2.de.debian.org;
+ 
+# destination directory
+DEST=$HOME/mirror/debian
+ 
+# Debian version(s) to mirror
+DIST=stretch
+ 
+# architecture
+ARCH=i386,amd64
+ 
+# log timestamp
+logger -t mirror[$$] updating Debian mirror
+ 
+su root -c \
+ "debmirror ${DEST} \
+ --nosource \
+ --host=${HOST} \
+ --root=/debian \
+ --dist=${DIST} \
+ --section=main,contrib,non-free,main/debian-installer \
+ --i18n \
+ --arch=${ARCH} \
+ --passive --cleanup \
+ $VERBOSE"
+ 
+logger -t mirror[$$] finished updating Debian mirror
